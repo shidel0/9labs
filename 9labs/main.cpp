2395811aed521a77f6c573a95b757afd8f58c48d
bael almazbekov <bael.almazbekov@gmail.com>
2025-04-22 11:42:21 +0600

    Добавьте файлы проекта.

---
+++ 9labs/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world from bekjan";
+	std::cout << "Hello world from bekjan";
+	std::cout << "Hello world from bekjan";
+	std::cout << "Hello world from bekjan";
+}
